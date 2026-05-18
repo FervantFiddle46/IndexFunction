25f097b44175c3759df3a92ef89b0bfe9e887c80
FervantFiddle46 <kirilleliseev61@gmail.com>
2026-05-18 19:27:48 +0500

    Add

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,71 @@
 #include <iostream>
 
-int* addEll(int* arr, int& size, int index, int el) {
+template<typename T>
+T* allocateMemory() 
+{
+    return new T;
+}
+
+template<typename T>
+void freeMemory(T* ptr) 
+{
+    delete ptr;
+}
+
+int* inArr(int size) 
+{
+    if (size <= 0) return nullptr;
+    return new int[size]();
+}
+
+template<typename T>
+void printArray(T arr[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n\n";
+}
+
+void delArr(int*& arr)
+{
+    delete[] arr;
+    arr = nullptr;
+}
+
+template<typename ArrType>
+inline void ArrPushBack(ArrType*& arr, int arrSize)
+{
+    arrSize++;
+    ArrType* temp = new ArrType[arrSize];
+    for (size_t i = 0; i < arrSize - 1; i++)
+    {
+        temp[i] = arr[i];
+    }
+    std::swap(arr, temp);
+    delete[]temp;
+
+}
+
+int* addEll(int* arr, int& size, int index, int el) 
+{
     if (index < 0 || index > size) return arr;
 
     int* newArr = new int[size + 1];
 
-    for (int i = 0; i < index; ++i) {
+    for (int i = 0; i < index; ++i) 
+    {
         newArr[i] = arr[i];
     }
     newArr[index] = el;
-    for (int i = index; i < size; ++i) {
+    for (int i = index; i < size; ++i) 
+    {
         newArr[i + 1] = arr[i];
     }
 
-    if (arr != nullptr) {
+    if (arr != nullptr) 
+    {
         delete[] arr;
     }
 
@@ -21,10 +73,12 @@ int* addEll(int* arr, int& size, int index, int el) {
     return newArr;
 }
 
-int* delEll(int* arr, int& size, int index) {
+int* delEll(int* arr, int& size, int index)
+{
     if (index < 0 || index >= size) return arr;
 
-    if (size == 1) {
+    if (size == 1) 
+    {
         delete[] arr;
         size = 0;
         return nullptr;
@@ -32,10 +86,12 @@ int* delEll(int* arr, int& size, int index) {
 
     int* newArr = new int[size - 1];
 
-    for (int i = 0; i < index; ++i) {
+    for (int i = 0; i < index; ++i) 
+    {
         newArr[i] = arr[i];
     }
-    for (int i = index + 1; i < size; ++i) {
+    for (int i = index + 1; i < size; ++i) 
+    {
         newArr[i - 1] = arr[i];
     }
 
@@ -48,53 +104,6 @@ int main()
 {
     int* arr = nullptr;
     int size = 0;
-
-    arr = addEll(arr, size, 0, 23);
-
-
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << "\n\n";
-
-    arr = addEll(arr, size, 0, 12);
-
-
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << "\n\n";
-
-    arr = addEll(arr, size, 1, 234);
-
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << "\n\n";
-    
-
-    arr = delEll(arr, size, 1);
-
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << "\n\n";
-
-    arr = delEll(arr, size, 1);
-
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << "\n\n";
-
-    arr = delEll(arr, size, 1);
-
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << "\n\n";
-
-
 
     delete[] arr;
 
